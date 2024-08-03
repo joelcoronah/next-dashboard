ca8aa932488d61d549fcbc2ab897245b808ef0ee
joelcoronah <joelcoronah@gmail.com>
2024-08-03 18:10:19 -0400

    :sparkles: feat: add pokemon list

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import React from "react";
-import { IoBrowsersOutline, IoCalculator, IoLogoReact } from "react-icons/io5";
+import {
+  IoBrowsersOutline,
+  IoCalculator,
+  IoFootball,
+  IoLogoReact,
+} from "react-icons/io5";
 import { SidebarMenuItem } from "./SidebarMenuItem";
 
 const menuItems = [
@@ -14,7 +19,13 @@ const menuItems = [
     path: "/dashboard/counter",
     icon: <IoCalculator size={40} />,
     title: "Counter",
-    subTitle: "VContador Client Side",
+    subTitle: "Contador Client Side",
+  },
+  {
+    path: "/dashboard/pokemons",
+    icon: <IoFootball size={40} />,
+    title: "Pokemons",
+    subTitle: "Pantalla de pokemons",
   },
 ];
 
@@ -23,7 +34,7 @@ export const Sidebar = () => {
     <div
       id="menu"
       style={{ width: "400px" }}
-      className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen overflow-y-scroll"
+      className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 overflow-y-scroll"
     >
       <div id="logo" className="my-4 px-6">
         <h1 className="flex items-center text-lg md:text-2xl font-bold text-white">
@@ -63,35 +74,6 @@ export const Sidebar = () => {
             subTitle={item.subTitle}
           />
         ))}
-        <a
-          href="#"
-          className="w-full px-2 inline-flex space-x-2 items-center border-b border-slate-700 py-3 hover:bg-white/5 transition ease-linear duration-150"
-        >
-          <div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"
-              />
-            </svg>
-          </div>
-          <div className="flex flex-col">
-            <span className="text-lg text-slate-300 font-bold leading-5">
-              Database
-            </span>
-            <span className="text-sm text-slate-500 hidden md:block">
-              Database Manager
-            </span>
-          </div>
-        </a>
       </div>
     </div>
   );
---
+++ src/app/pokemons/index.ts
@@ -0,0 +1,3 @@
+export { PokemonGrid } from "./components/PokemonGrid";
+export type { PokemonsResponse } from "./interfaces/pokemos-response";
+export type { SimplePokemon } from "./interfaces/simple-pokemon";
